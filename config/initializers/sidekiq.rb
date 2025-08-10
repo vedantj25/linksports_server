0aea4dc1846e5ce8c3eeceac667d4e63f7338618
vedantjoshi25 <vedant.joshi@branch.co>
2025-08-10 11:42:31 +0530

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,9 @@
 redis_url = ENV["REDIS_URL"] || ENV["REDIS_TLS_URL"] || ENV["UPSTASH_REDIS_URL"] || "redis://localhost:6379/0"
-redis_namespace = ENV["SIDEKIQ_NAMESPACE"] || "sidekiq"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url, namespace: redis_namespace }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url, namespace: redis_namespace }
+  config.redis = { url: redis_url }
 end
